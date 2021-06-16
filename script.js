0115ece18b7a833bc26d30099db8b0b20977bb6d
Parthbajaj <46967342+Parthbajaj@users.noreply.github.com>
2021-06-16 23:32:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
+var mybutton = document.getElementById("myBtn");
 var sticky = navbar.offsetTop;
 
+
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+if (window.pageYOffset >= sticky) {
+  navbar.classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+  navbar.classList.remove("sticky");
   }
-}
 
 
-// scroll to top
-
-//Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
@@ -28,8 +20,9 @@ function scrollFunction() {
   }
 }
 
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
